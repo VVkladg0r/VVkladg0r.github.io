4df84c74b7ba5874aef619cfae17370cceae36a3
kuailedaigua <798973926@qq.com>
2024-05-13 13:37:08 +0800

    Site updated: 2024-05-13 13:37:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/02/hello-world/","2024/05/13/LDAP注入/","2024/05/13/java基础/","2024/05/13/SQL注入/","2024/05/13/php反序列化/","2024/05/13/pickle反序列化/","2024/05/13/远程命令执行/","2024/05/12/博客搭建/"];function toRandomPost(){
+var posts=["2024/05/02/hello-world/","2024/05/13/LDAP注入/","2024/05/13/SQL注入/","2024/05/13/php反序列化/","2024/05/12/博客搭建/","2024/05/13/java基础/","2024/05/13/pickle反序列化/","2024/05/13/远程命令执行/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
